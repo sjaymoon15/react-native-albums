c7d1dc3ec9214fb38f4bd428d52d019d1fc3e2e6
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-19 18:31:08 -0500

    fetch albumlist

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(res => res.json())
+    .then(res => this.setState({ albums: res }));
+  }
+  renderAlbums() {
+    return this.state.albums.map(album => <Text key={album.url}>{album.title}</Text>);
+  }
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album list</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
